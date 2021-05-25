57cdd8dbf780b4f9507f17a5a2e13bec5055b9cd
Josef Friedrich <josef@friedrich.rocks>
2021-05-25 21:30:07 +0200

    Test speed function

--- test/test-script.sh
+++ test/test-script.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-_zpool_status () {
+_get_zpool_status_stdout () {
 echo '  pool: zfsdata
  state: ONLINE
   scan: scrub in progress since Sat Aug  3 10:21:02 2019
@@ -17,24 +17,24 @@ config:
 }
 
 
-_grab_time_to_go() {
-  local MINUTES HOURS DAYS
-  _get_line() {
-    _zpool_status | grep ' to go'
-  }
-  if [ -n "$(_zpool_status | grep 'days .*:.*:.*')" ]; then
-    DAYS="$(_get_line | sed -E 's/^.* (.*) days .*$/\1/g')"
-    HOURS="$(_get_line | sed -E 's/^.*days (..):.*$/\1/g')"
-    MINUTES="$(_get_line | sed -E 's/^.*days ..:(..):.*$/\1/g')"
-    echo $((DAYS * 1440 + HOURS * 60 + MINUTES))
-	elif [ -n "$(_zpool_status | grep 'h.*m) to go')" ]; then
-    HOURS=$(_get_line | sed 's/h.*//')
-    MINUTES=$(_get_line | sed 's/.*h//' | sed 's/m//')
-    echo $((HOURS * 60 + MINUTES))
+_speed() {
+	local SPEED UNIT
+	SPEED="$(_get_zpool_status_stdout "$1" | grep -E -o '[[:digit:],]*[[:alpha:]]/s')"
+  echo $SPEED
+
+	if [ -n "$SPEED" ]; then
+    SPEED=$(echo "$1" | sed 's#/s##' | tr , .)
+    UNIT=$(echo -n "$SPEED" | tail -c 1)
+    SPEED=$(echo "$SPEED" | sed 's/.$//' )
+
+    if [ "$UNIT" = K ]; then
+      SPEED="$(echo "$SPEED" | \
+        awk '{MB = $1 / 1024 ; print MB}')"
+    fi
+    echo $SPEED
 	else
 		echo 0
 	fi
 }
 
-
-_grab_time_to_go
+_speed
