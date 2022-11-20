dc75ecfdd9dbe7a3d8602d27f75f440e79837bf2
Josef Friedrich <josef@friedrich.rocks>
2022-11-20 21:00:12 +0100

    Add description and epilog to the argparser

--- check_zpool_scrub.py
+++ check_zpool_scrub.py
@@ -1,7 +1,6 @@
 #! /usr/bin/env python3
 
 import argparse
-from argparse import ArgumentParser
 from nagiosplugin.runtime import guarded
 from typing import cast
 
@@ -13,9 +12,32 @@ class OptionContainer:
 opts: OptionContainer = OptionContainer()
 
 
-def get_argparser() -> ArgumentParser:
-    parser: ArgumentParser = argparse.ArgumentParser(
+def get_argparser() -> argparse.ArgumentParser:
+    parser: argparse.ArgumentParser = argparse.ArgumentParser(
         prog="check_zpool_scrub",  # To get the right command name in the README.
+        formatter_class=lambda prog: argparse.RawDescriptionHelpFormatter(
+            prog, width=80
+        ),  # noqa: E501
+        description="Copyright (c) 2016-22 Josef Friedrich <josef@friedrich.rocks>\n"
+        "\n"
+        "Monitoring plugin to check how long ago the last ZFS scrub was performed.\n",  # noqa: E501
+        epilog="Performance data:\n"
+        "\n"
+        "POOL is the name of the pool\n"
+        "\n"
+        " - POOL_last_ago\n"
+        "    Time interval in seconds for last scrub.\n"
+        " - POOL_progress\n"
+        "    Percent 0 - 100\n"
+        " - POOL_speed\n"
+        "    MB per second.\n"
+        " - POOL_time\n"
+        "    Time to go in seconds.\n"
+        "\n"
+        "Details about the implementation of this monitoring plugin:\n"
+        "\n"
+        "This monitoring plugin grabs the last scrub date from the command\n"
+        "'zpool status POOL'.\n",
     )
 
     parser.add_argument(
