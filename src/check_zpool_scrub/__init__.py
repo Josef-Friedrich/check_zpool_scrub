8cab5eba8fcc2568391037e7032c926b074be8dd
Josef Friedrich <josef@friedrich.rocks>
2026-03-01 10:38:05 +0100

    Use logging from the mplugin

--- src/check_zpool_scrub/__init__.py
+++ src/check_zpool_scrub/__init__.py
@@ -3,7 +3,6 @@
 from __future__ import annotations
 
 import argparse
-import logging
 import re
 import subprocess
 import typing
@@ -20,6 +19,7 @@ from mplugin import (
     Resource,
     Result,
     guarded,
+    log,
     setup_argparser,
     timespan,
 )
@@ -36,98 +36,6 @@ class OptionContainer:
 
 
 opts: OptionContainer = OptionContainer()
-
-
-class Logger:
-    """A wrapper around the Python logging module with 3 debug logging levels.
-
-    1. ``-d``: info
-    2. ``-dd``: debug
-    3. ``-ddd``: verbose
-    """
-
-    __logger: logging.Logger
-
-    __BLUE = "\x1b[0;34m"
-    __PURPLE = "\x1b[0;35m"
-    __CYAN = "\x1b[0;36m"
-    __RESET = "\x1b[0m"
-
-    __INFO = logging.INFO
-    __DEBUG = logging.DEBUG
-    __VERBOSE = 5
-
-    def __init__(self) -> None:
-        handler = logging.StreamHandler()
-        handler.setFormatter(logging.Formatter("%(message)s"))
-        logging.basicConfig(handlers=[handler])
-        self.__logger = logging.getLogger(__name__)
-
-    def set_level(self, level: int) -> None:
-        # NOTSET=0
-        # custom level: VERBOSE=5
-        # DEBUG=10
-        # INFO=20
-        # WARN=30
-        # ERROR=40
-        # CRITICAL=50
-        if level == 1:
-            self.__logger.setLevel(logging.INFO)
-        elif level == 2:
-            self.__logger.setLevel(logging.DEBUG)
-        elif level > 2:
-            self.__logger.setLevel(5)
-
-    def __log(self, level: int, color: str, msg: str, *args: object) -> None:
-        a: list[str] = []
-        for arg in args:
-            a.append(color + str(arg) + self.__RESET)
-        self.__logger.log(level, msg, *a)
-
-    def info(self, msg: str, *args: object) -> None:
-        """Log on debug level ``1``: ``-d``.
-
-        :param msg: A message format string. Note that this means that you can
-            use keywords in the format string, together with a single
-            dictionary argument. No ``%`` formatting operation is performed on
-            ``msg`` when no args are supplied.
-        :param args: The arguments which are merged into ``msg`` using the
-            string formatting operator.
-        """
-        self.__log(self.__INFO, self.__BLUE, msg, *args)
-
-    def debug(self, msg: str, *args: object) -> None:
-        """Log on debug level ``2``: ``-dd``.
-
-        :param msg: A message format string. Note that this means that you can
-            use keywords in the format string, together with a single
-            dictionary argument. No ``%`` formatting operation is performed on
-            ``msg`` when no args are supplied.
-        :param args: The arguments which are merged into ``msg`` using the
-            string formatting operator.
-        """
-        self.__log(self.__DEBUG, self.__PURPLE, msg, *args)
-
-    def verbose(self, msg: str, *args: object) -> None:
-        """Log on debug level ``3``: ``-ddd``
-
-        :param msg: A message format string. Note that this means that you can
-            use keywords in the format string, together with a single
-            dictionary argument. No ``%`` formatting operation is performed on
-            ``msg`` when no args are supplied.
-        :param args: The arguments which are merged into ``msg`` using the
-            string formatting operator.
-        """
-        self.__log(self.__VERBOSE, self.__CYAN, msg, *args)
-
-    def show_levels(self) -> None:
-        msg = "log level %s (%s): %s"
-        self.info(msg, 1, "info", "-d")
-        self.debug(msg, 2, "debug", "-dd")
-        self.verbose(msg, 3, "verbose", "-ddd")
-
-
-logger = Logger()
 
 
 def _list_pools() -> list[str]:
@@ -147,7 +55,7 @@ def _list_pools() -> list[str]:
         .strip()
         .splitlines()
     )
-    logger.verbose("Output from %s: %s", "zpool list -H -o name", pools)
+    log.debug("Output from %s: %s", "zpool list -H -o name", pools)
     return pools
 
 
@@ -162,7 +70,7 @@ class PoolScrubStatus:
         self.__zpool_status_output = subprocess.check_output(
             ["zpool", "status", pool], encoding="UTF-8"
         )
-        logger.verbose("Output from %s: %s", "zpool status", self.__zpool_status_output)
+        log.debug("Output from %s: %s", "zpool status", self.__zpool_status_output)
 
     @property
     def progress(self) -> Optional[float]:
@@ -413,7 +321,7 @@ def main() -> None:
 
     check: Check = Check(*checks)
     check.name = "zpool_scrub"
-    check.main(opts.verbose)
+    check.main(verbose=opts.verbose)
 
 
 if __name__ == "__main__":
